b04ced664ebba1b9497910eb86dbc1a6c0b0fa1e
Dania Multi Prima <daniamultiprima@gmail.com>
2026-08-03 11:09:50 +0700

    feat: add more SEO keywords

--- app/layout.js
+++ app/layout.js
@@ -49,40 +49,107 @@ export const metadata = {
   
   // Keywords sangat penting untuk pencarian
   keywords: [
-    // --- LSI & VARIANT UMUM (Broad Topics) ---
-    "IT Consultant Medan", 
-    "IT Consultant Deli Serdang", 
-    "Konsultan IT Bersertifikat Indonesia", 
-    "Jasa Pembuatan Aplikasi Pemerintahan (SPBE)", 
-    "AI Developer Medan", 
-    "Jasa Machine Learning & AI Indonesia", 
-    "Software House Terpercaya di Sumatera Utara", 
+    // --- SKALA NASIONAL (INDONESIA) ---
+    "Konsultan IT di Indonesia", "Konsultan IT Terbaik di Indonesia", "Perusahaan IT di Indonesia", "Jasa Konsultan IT Indonesia",
+    "Software House Indonesia", "Software House Terbaik di Indonesia", "Jasa Programmer Indonesia", "Programmer Profesional Indonesia",
+    "Jasa Pembuatan Aplikasi Indonesia", "Vendor IT Seluruh Indonesia", "Konsultan Teknologi Informasi Indonesia",
     
-    // --- HEALTHCARE & GOV SPECIFIC ---
-    "Sistem Informasi Manajemen Rumah Sakit (SIMRS) Terbaik", 
-    "Smart City Solutions Indonesia",
-    "Aplikasi Rekam Medis Elektronik (RME)",
-    "Vendor E-Katalog Pemerintah Sumut",
+    // --- PEMERINTAH & BUMN (GOV & ENTERPRISE) ---
+    "Konsultan IT Pemerintah", "Konsultan IT Pemerintahan", "Vendor IT Pemerintahan", 
+    "Jasa Pembuatan Aplikasi SPBE", "Aplikasi Smart City Pemerintah", "Software House Pemerintahan", 
+    "Pengembang Sistem E-Government Indonesia", "Konsultan Teknologi Pemerintahan Indonesia", 
+    "Vendor E-Katalog Pemerintah Daerah", "Vendor E-Katalog Nasional", "Jasa Software House BUMN dan Pemerintah", 
+    "Sistem Informasi Pemerintahan", "Pembuatan Aplikasi SPBE Pemerintahan",
+    
+    // --- KOTA & PROVINSI DI INDONESIA (SUMATERA) ---
+    "Konsultan IT Aceh", "Software House Aceh", "Vendor IT Banda Aceh",
+    "Konsultan IT Medan", "Software House Medan", "Programmer Medan", "Vendor IT Sumatera Utara",
+    "Konsultan IT Padang", "Software House Padang", "Vendor IT Sumatera Barat",
+    "Konsultan IT Pekanbaru", "Software House Riau",
+    "Konsultan IT Batam", "Software House Kepulauan Riau", "Konsultan IT Tanjungpinang",
+    "Konsultan IT Jambi", "Konsultan IT Palembang", "Software House Sumatera Selatan",
+    "Konsultan IT Bengkulu", "Konsultan IT Bangka Belitung", "Konsultan IT Pangkalpinang", 
+    "Konsultan IT Bandar Lampung", "Software House Lampung",
+    
+    // --- KOTA & PROVINSI DI INDONESIA (JAWA & BALI) ---
+    "Konsultan IT Jakarta", "Software House Jakarta", "Vendor IT DKI Jakarta",
+    "Konsultan IT Serang", "Konsultan IT Banten", "Konsultan IT Tangerang",
+    "Konsultan IT Bandung", "Software House Bandung", "Vendor IT Jawa Barat",
+    "Konsultan IT Semarang", "Software House Semarang", "Vendor IT Jawa Tengah",
+    "Konsultan IT Yogyakarta", "Software House Jogja", "Vendor IT DIY",
+    "Konsultan IT Surabaya", "Software House Surabaya", "Vendor IT Jawa Timur", "Konsultan IT Malang", 
+    "Konsultan IT Denpasar", "Konsultan IT Bali", "Software House Bali",
+    
+    // --- KOTA & PROVINSI DI INDONESIA (KALIMANTAN) ---
+    "Konsultan IT Pontianak", "Konsultan IT Kalimantan Barat",
+    "Konsultan IT Palangkaraya", "Konsultan IT Kalimantan Tengah",
+    "Konsultan IT Banjarmasin", "Konsultan IT Banjarbaru", "Konsultan IT Kalimantan Selatan",
+    "Konsultan IT Samarinda", "Konsultan IT Balikpapan", "Konsultan IT Kalimantan Timur",
+    "Konsultan IT IKN Nusantara", "Konsultan IT Tarakan", "Konsultan IT Kalimantan Utara",
+    
+    // --- KOTA & PROVINSI DI INDONESIA (SULAWESI) ---
+    "Konsultan IT Makassar", "Software House Makassar", "Vendor IT Sulawesi Selatan",
+    "Konsultan IT Manado", "Konsultan IT Sulawesi Utara",
+    "Konsultan IT Palu", "Konsultan IT Sulawesi Tengah",
+    "Konsultan IT Kendari", "Konsultan IT Sulawesi Tenggara",
+    "Konsultan IT Gorontalo", "Konsultan IT Mamuju", "Konsultan IT Sulawesi Barat",
+    
+    // --- KOTA & PROVINSI DI INDONESIA (NUSA TENGGARA, MALUKU, PAPUA) ---
+    "Konsultan IT Mataram", "Konsultan IT NTB", "Nusa Tenggara Barat",
+    "Konsultan IT Kupang", "Konsultan IT NTT", "Nusa Tenggara Timur",
+    "Konsultan IT Ambon", "Konsultan IT Maluku",
+    "Konsultan IT Ternate", "Konsultan IT Maluku Utara",
+    "Konsultan IT Jayapura", "Konsultan IT Papua", 
+    "Konsultan IT Sorong", "Konsultan IT Papua Barat", 
+    "Konsultan IT Manokwari", "Konsultan IT Merauke", "Konsultan IT Nabire", "Konsultan IT Wamena",
+    
+    // --- LSI & VARIANT UMUM (Broad Topics) ---
+    "IT Consultant Medan", "IT Consultant Deli Serdang", 
+    "Konsultan IT Bersertifikat Indonesia", "Jasa Pembuatan Aplikasi Pemerintahan (SPBE)", 
+    "Software House Terpercaya di Sumatera Utara",
+    
+    // --- FOKUS AI & KECERDASAN BUATAN (LOKAL & NASIONAL) ---
+    "AI Developer Medan", "AI Developer Indonesia", "Jasa Machine Learning & AI Indonesia", 
+    "Perusahaan AI di Jakarta", "Perusahaan AI di Medan", "Perusahaan AI di Indonesia",
+    "Konsultan Kecerdasan Buatan di Medan", "Konsultan Kecerdasan Buatan di Jakarta",
+    "Konsultan Kecerdasan Buatan Indonesia", "Jasa Pembuatan AI Indonesia",
+    "Vendor Kecerdasan Buatan", "Vendor AI Indonesia", "Vendor AI Jakarta", "Vendor AI Medan",
+    "Jasa Pembuatan Chatbot AI", "Jasa Chatbot AI Medan", "Jasa Chatbot AI Jakarta", 
+    "Jasa Pembuatan Chatbot WhatsApp AI",
+    "Jasa Machine Learning Indonesia", "Jasa Machine Learning Medan", "Jasa Machine Learning Jakarta",
+    "Pengembang AI Sumatera Utara", "Pengembang AI Deli Serdang", 
+    "AI Developer Jakarta", "AI Developer Surabaya", "AI Developer Bandung", "AI Developer Bali",
+    "Perusahaan Artificial Intelligence Indonesia", "Solusi AI untuk Perusahaan",
+    "Jasa Integrasi AI ChatGPT", "Konsultan Implementasi AI", 
+    "Jasa Pembuatan Software AI Indonesia", "Jasa Pembuatan Sistem AI",
+    
+    // --- FOKUS APLIKASI & TEKNOLOGI SPESIFIK (LOKAL & NASIONAL) ---
+    "Konsultan Aplikasi Medan", "Konsultan Aplikasi Jakarta", "Konsultan Aplikasi Indonesia",
+    "Jasa Pembuatan Aplikasi Medan", "Jasa Pembuatan Aplikasi Jakarta", "Jasa Pembuatan Aplikasi Custom Indonesia",
+    "Vendor SIMRS Jakarta", "SIMRS Jakarta", "Vendor SIMRS Medan", "Sistem Informasi Rumah Sakit Indonesia",
+    "Aplikasi Rekam Medis Medan", "Aplikasi Rekam Medis Jakarta", "Vendor E-Rekam Medis Indonesia",
+    "Vendor Aplikasi ERP Jakarta", "Vendor Aplikasi ERP Medan", "Jasa Pembuatan ERP Indonesia",
+    "Pembuatan Aplikasi Keuangan", "Sistem e-Ticketing Indonesia", "Jasa Pembuatan Aplikasi Koperasi",
+    "Jasa IoT Jakarta", "Jasa Internet of Things Medan", "Konsultan IoT Indonesia",
+    "Software House SPBE Jakarta", "Konsultan SPBE Jakarta", "Konsultan SPBE Indonesia",
+    "Aplikasi Perkantoran Jakarta", "Sistem HRD Custom Indonesia", "Vendor Aplikasi Payroll Indonesia",
+    
+    // --- HEALTHCARE SPECIFIC ---
+    "Sistem Informasi Manajemen Rumah Sakit (SIMRS) Terbaik", "Smart City Solutions Indonesia",
+    "Aplikasi Rekam Medis Elektronik (RME)", "Vendor E-Katalog Pemerintah Sumut",
     
     // --- CORPORATE IDENTITY ---
-    "PT Dania Multi Prima",
-    "PT DMP",
-    "DMP IT Consultant",
+    "PT Dania Multi Prima", "PT DMP", "DMP IT Consultant",
     
     // --- HIGH-INTENT SERVICES ---
-    "Jasa Pembuatan Website Perusahaan Medan",
-    "Jasa Pembuatan Aplikasi Mobile Android iOS",
-    "Jasa Bikin Web Toko Online",
+    "Jasa Pembuatan Website Perusahaan Medan", "Jasa Pembuatan Website Perusahaan Indonesia", 
+    "Jasa Pembuatan Aplikasi Mobile Android iOS", "Jasa Bikin Web Toko Online", "Jasa IT Custom Seluruh Indonesia",
     
     // --- BRIDGING & INTEGRATION ---
-    "Jasa Bridging BPJS V-Claim & PCare",
-    "Integrasi SATUSEHAT Kemenkes",
-    "Pembuatan API Payment Gateway",
+    "Jasa Bridging BPJS V-Claim & PCare", "Integrasi SATUSEHAT Kemenkes", "Pembuatan API Payment Gateway",
     
     // --- ERP & CUSTOM SOFTWARE ---
-    "Pembuatan Software ERP Custom",
-    "Aplikasi Kasir POS Terintegrasi",
-    "Aplikasi E-Office Tata Naskah Dinas"
+    "Pembuatan Software ERP Custom", "Aplikasi Kasir POS Terintegrasi", "Aplikasi E-Office Tata Naskah Dinas"
   ],
 
   authors: [{ name: "PT. Dania Multi Prima" }],
--- app/page.js
+++ app/page.js
@@ -23,52 +23,107 @@ export const metadata = {
   authors: [{ name: 'PT. Dania Multi Prima', url: 'https://dmp.bio' }],
   generator: 'Next.js',
   keywords: [
-    // --- KATA KUNCI UTAMA (High Volume) ---
-    'Jasa Pembuatan Website',
-    'Jasa Pembuatan Aplikasi Android',
-    'Jasa Pembuatan Aplikasi iOS',
-    'Software House Indonesia',
-    'Software House Terbaik Indonesia',
-    'Perusahaan IT BUMN',
-    'Jasa Bikin Web Profil Perusahaan',
-    'Jasa Pembuatan Aplikasi Kasir',
+    // --- SKALA NASIONAL (INDONESIA) ---
+    "Konsultan IT di Indonesia", "Konsultan IT Terbaik di Indonesia", "Perusahaan IT di Indonesia", "Jasa Konsultan IT Indonesia",
+    "Software House Indonesia", "Software House Terbaik di Indonesia", "Jasa Programmer Indonesia", "Programmer Profesional Indonesia",
+    "Jasa Pembuatan Aplikasi Indonesia", "Vendor IT Seluruh Indonesia", "Konsultan Teknologi Informasi Indonesia",
     
-    // --- LOKAL MEDAN & SUMUT (Dominasi Lokal High Intent) ---
-    'Software House Medan',
-    'Jasa Pembuatan Aplikasi Medan',
-    'Jasa Pembuatan Website Medan',
-    'Konsultan IT Medan',
-    'Programmer Medan',
-    'Web Developer Medan',
-    'Vendor IT Deli Serdang',
-    'Jasa IT Support Medan',
-    'Pembuatan Aplikasi Android Deli Serdang',
-    'Perusahaan IT Terpercaya di Medan',
-    'Jasa Web Design Medan',
+    // --- PEMERINTAH & BUMN (GOV & ENTERPRISE) ---
+    "Konsultan IT Pemerintah", "Konsultan IT Pemerintahan", "Vendor IT Pemerintahan", 
+    "Jasa Pembuatan Aplikasi SPBE", "Aplikasi Smart City Pemerintah", "Software House Pemerintahan", 
+    "Pengembang Sistem E-Government Indonesia", "Konsultan Teknologi Pemerintahan Indonesia", 
+    "Vendor E-Katalog Pemerintah Daerah", "Vendor E-Katalog Nasional", "Jasa Software House BUMN dan Pemerintah", 
+    "Sistem Informasi Pemerintahan", "Pembuatan Aplikasi SPBE Pemerintahan",
     
-    // --- ENTERPRISE, GOV & HEALTH (Niche Khusus) ---
-    'Sistem Informasi Manajemen Rumah Sakit (SIMRS)',
-    'Vendor SIMRS Terpercaya',
-    'Aplikasi Smart City Indonesia',
-    'Vendor Aplikasi SPBE Pemerintahan',
-    'Sistem E-Katalog Pemerintah',
-    'Vendor E-Katalog Lokal',
-    'Software Klinik dan Apotek Terintegrasi',
+    // --- KOTA & PROVINSI DI INDONESIA (SUMATERA) ---
+    "Konsultan IT Aceh", "Software House Aceh", "Vendor IT Banda Aceh",
+    "Konsultan IT Medan", "Software House Medan", "Programmer Medan", "Vendor IT Sumatera Utara",
+    "Konsultan IT Padang", "Software House Padang", "Vendor IT Sumatera Barat",
+    "Konsultan IT Pekanbaru", "Software House Riau",
+    "Konsultan IT Batam", "Software House Kepulauan Riau", "Konsultan IT Tanjungpinang",
+    "Konsultan IT Jambi", "Konsultan IT Palembang", "Software House Sumatera Selatan",
+    "Konsultan IT Bengkulu", "Konsultan IT Bangka Belitung", "Konsultan IT Pangkalpinang", 
+    "Konsultan IT Bandar Lampung", "Software House Lampung",
     
-    // --- TECH STACK & SERVICES (Advanced Tech) ---
-    'Jasa Integrasi API',
-    'Jasa Integrasi API Payment Gateway',
-    'Pengembang AI Indonesia',
-    'Internet of Things (IoT) Solutions',
-    'Jasa Pembuatan Chatbot AI',
-    'Implementasi Machine Learning Perusahaan',
-    'Konsultan IT Bersertifikat',
+    // --- KOTA & PROVINSI DI INDONESIA (JAWA & BALI) ---
+    "Konsultan IT Jakarta", "Software House Jakarta", "Vendor IT DKI Jakarta",
+    "Konsultan IT Serang", "Konsultan IT Banten", "Konsultan IT Tangerang",
+    "Konsultan IT Bandung", "Software House Bandung", "Vendor IT Jawa Barat",
+    "Konsultan IT Semarang", "Software House Semarang", "Vendor IT Jawa Tengah",
+    "Konsultan IT Yogyakarta", "Software House Jogja", "Vendor IT DIY",
+    "Konsultan IT Surabaya", "Software House Surabaya", "Vendor IT Jawa Timur", "Konsultan IT Malang", 
+    "Konsultan IT Denpasar", "Konsultan IT Bali", "Software House Bali",
     
-    // --- LONG TAIL & BUYING INTENT ---
-    'Harga Jasa Pembuatan Aplikasi Android Perusahaan',
-    'Biaya Pembuatan Website Toko Online',
-    'Jasa IT Konsultan Profesional',
-    'Bikin Aplikasi Custom Murah Berkualitas'
+    // --- KOTA & PROVINSI DI INDONESIA (KALIMANTAN) ---
+    "Konsultan IT Pontianak", "Konsultan IT Kalimantan Barat",
+    "Konsultan IT Palangkaraya", "Konsultan IT Kalimantan Tengah",
+    "Konsultan IT Banjarmasin", "Konsultan IT Banjarbaru", "Konsultan IT Kalimantan Selatan",
+    "Konsultan IT Samarinda", "Konsultan IT Balikpapan", "Konsultan IT Kalimantan Timur",
+    "Konsultan IT IKN Nusantara", "Konsultan IT Tarakan", "Konsultan IT Kalimantan Utara",
+    
+    // --- KOTA & PROVINSI DI INDONESIA (SULAWESI) ---
+    "Konsultan IT Makassar", "Software House Makassar", "Vendor IT Sulawesi Selatan",
+    "Konsultan IT Manado", "Konsultan IT Sulawesi Utara",
+    "Konsultan IT Palu", "Konsultan IT Sulawesi Tengah",
+    "Konsultan IT Kendari", "Konsultan IT Sulawesi Tenggara",
+    "Konsultan IT Gorontalo", "Konsultan IT Mamuju", "Konsultan IT Sulawesi Barat",
+    
+    // --- KOTA & PROVINSI DI INDONESIA (NUSA TENGGARA, MALUKU, PAPUA) ---
+    "Konsultan IT Mataram", "Konsultan IT NTB", "Nusa Tenggara Barat",
+    "Konsultan IT Kupang", "Konsultan IT NTT", "Nusa Tenggara Timur",
+    "Konsultan IT Ambon", "Konsultan IT Maluku",
+    "Konsultan IT Ternate", "Konsultan IT Maluku Utara",
+    "Konsultan IT Jayapura", "Konsultan IT Papua", 
+    "Konsultan IT Sorong", "Konsultan IT Papua Barat", 
+    "Konsultan IT Manokwari", "Konsultan IT Merauke", "Konsultan IT Nabire", "Konsultan IT Wamena",
+    
+    // --- LSI & VARIANT UMUM (Broad Topics) ---
+    "IT Consultant Medan", "IT Consultant Deli Serdang", 
+    "Konsultan IT Bersertifikat Indonesia", "Jasa Pembuatan Aplikasi Pemerintahan (SPBE)", 
+    "Software House Terpercaya di Sumatera Utara",
+    
+    // --- FOKUS AI & KECERDASAN BUATAN (LOKAL & NASIONAL) ---
+    "AI Developer Medan", "AI Developer Indonesia", "Jasa Machine Learning & AI Indonesia", 
+    "Perusahaan AI di Jakarta", "Perusahaan AI di Medan", "Perusahaan AI di Indonesia",
+    "Konsultan Kecerdasan Buatan di Medan", "Konsultan Kecerdasan Buatan di Jakarta",
+    "Konsultan Kecerdasan Buatan Indonesia", "Jasa Pembuatan AI Indonesia",
+    "Vendor Kecerdasan Buatan", "Vendor AI Indonesia", "Vendor AI Jakarta", "Vendor AI Medan",
+    "Jasa Pembuatan Chatbot AI", "Jasa Chatbot AI Medan", "Jasa Chatbot AI Jakarta", 
+    "Jasa Pembuatan Chatbot WhatsApp AI",
+    "Jasa Machine Learning Indonesia", "Jasa Machine Learning Medan", "Jasa Machine Learning Jakarta",
+    "Pengembang AI Sumatera Utara", "Pengembang AI Deli Serdang", 
+    "AI Developer Jakarta", "AI Developer Surabaya", "AI Developer Bandung", "AI Developer Bali",
+    "Perusahaan Artificial Intelligence Indonesia", "Solusi AI untuk Perusahaan",
+    "Jasa Integrasi AI ChatGPT", "Konsultan Implementasi AI", 
+    "Jasa Pembuatan Software AI Indonesia", "Jasa Pembuatan Sistem AI",
+    
+    // --- FOKUS APLIKASI & TEKNOLOGI SPESIFIK (LOKAL & NASIONAL) ---
+    "Konsultan Aplikasi Medan", "Konsultan Aplikasi Jakarta", "Konsultan Aplikasi Indonesia",
+    "Jasa Pembuatan Aplikasi Medan", "Jasa Pembuatan Aplikasi Jakarta", "Jasa Pembuatan Aplikasi Custom Indonesia",
+    "Vendor SIMRS Jakarta", "SIMRS Jakarta", "Vendor SIMRS Medan", "Sistem Informasi Rumah Sakit Indonesia",
+    "Aplikasi Rekam Medis Medan", "Aplikasi Rekam Medis Jakarta", "Vendor E-Rekam Medis Indonesia",
+    "Vendor Aplikasi ERP Jakarta", "Vendor Aplikasi ERP Medan", "Jasa Pembuatan ERP Indonesia",
+    "Pembuatan Aplikasi Keuangan", "Sistem e-Ticketing Indonesia", "Jasa Pembuatan Aplikasi Koperasi",
+    "Jasa IoT Jakarta", "Jasa Internet of Things Medan", "Konsultan IoT Indonesia",
+    "Software House SPBE Jakarta", "Konsultan SPBE Jakarta", "Konsultan SPBE Indonesia",
+    "Aplikasi Perkantoran Jakarta", "Sistem HRD Custom Indonesia", "Vendor Aplikasi Payroll Indonesia",
+    
+    // --- HEALTHCARE SPECIFIC ---
+    "Sistem Informasi Manajemen Rumah Sakit (SIMRS) Terbaik", "Smart City Solutions Indonesia",
+    "Aplikasi Rekam Medis Elektronik (RME)", "Vendor E-Katalog Pemerintah Sumut",
+    
+    // --- CORPORATE IDENTITY ---
+    "PT Dania Multi Prima", "PT DMP", "DMP IT Consultant",
+    
+    // --- HIGH-INTENT SERVICES ---
+    "Jasa Pembuatan Website Perusahaan Medan", "Jasa Pembuatan Website Perusahaan Indonesia", 
+    "Jasa Pembuatan Aplikasi Mobile Android iOS", "Jasa Bikin Web Toko Online", "Jasa IT Custom Seluruh Indonesia",
+    
+    // --- BRIDGING & INTEGRATION ---
+    "Jasa Bridging BPJS V-Claim & PCare", "Integrasi SATUSEHAT Kemenkes", "Pembuatan API Payment Gateway",
+    
+    // --- ERP & CUSTOM SOFTWARE ---
+    "Pembuatan Software ERP Custom", "Aplikasi Kasir POS Terintegrasi", "Aplikasi E-Office Tata Naskah Dinas"
   ],
 
   
@@ -151,6 +206,9 @@ const jsonLd = [
       { "@type": "City", "name": "Medan" },
       { "@type": "City", "name": "Binjai" },
       { "@type": "City", "name": "Deli Serdang" },
+      { "@type": "City", "name": "Jakarta" },
+      { "@type": "City", "name": "Surabaya" },
+      { "@type": "City", "name": "Bandung" },
       { "@type": "AdministrativeArea", "name": "Sumatera Utara" },
       { "@type": "Country", "name": "Indonesia" }
     ],
@@ -175,7 +233,7 @@ const jsonLd = [
     
     "hasOfferCatalog": {
       "@type": "OfferCatalog",
-      "name": "Layanan IT & Software Development",
+      "name": "Layanan IT & Software Development Terlengkap",
       "itemListElement": [
         {
           "@type": "Offer",
@@ -195,14 +253,35 @@ const jsonLd = [
           "@type": "Offer",
           "itemOffered": {
             "@type": "Service",
-            "name": "Sistem Informasi Manajemen Rumah Sakit (SIMRS)"
+            "name": "Sistem Informasi Manajemen Rumah Sakit (SIMRS) & RME"
           }
         },
         {
           "@type": "Offer",
           "itemOffered": {
             "@type": "Service",
-            "name": "Konsultan IT & Smart City Pemerintahan"
+            "name": "Konsultan IT, SPBE & Smart City Pemerintahan"
+          }
+        },
+        {
+          "@type": "Offer",
+          "itemOffered": {
+            "@type": "Service",
+            "name": "Pengembangan Artificial Intelligence (AI) & Machine Learning"
+          }
+        },
+        {
+          "@type": "Offer",
+          "itemOffered": {
+            "@type": "Service",
+            "name": "Pembuatan Software Custom (ERP, HRD, Keuangan)"
+          }
+        },
+        {
+          "@type": "Offer",
+          "itemOffered": {
+            "@type": "Service",
+            "name": "Internet of Things (IoT) Solutions"
           }
         }
       ]
